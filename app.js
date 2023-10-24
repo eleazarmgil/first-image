c437bb17c3e7652fcbe0172f69c0999493ca8856
Maria Vasquez <80843284+magshva@users.noreply.github.com>
2023-10-23 23:54:11 -0400

    Develop (#4)
    * Default config (#1)
    
    * Added initial config
    
    * Creating a connection with MongoDB
    
    ---------
    
    Co-authored-by: eleazarmgil <maug,lucas@gmail.com>
    
    * Added put and delete endpoints (#2)
    
    Co-authored-by: eleazarmgil <maug,lucas@gmail.com>
    
    * Docker (#3)
    
    * Added Dockerfile
    
    * Creating docker compose
    
    * saving changes in dockercompose
    
    * Added patch endpoint
    
    * Fixed patch endpoint
    
    * adding requests
    
    ---------
    
    Co-authored-by: eleazarmgil <maug,lucas@gmail.com>
    
    ---------
    
    Co-authored-by: eleazarmgil <maug,lucas@gmail.com>
    Co-authored-by: Eleazar Gil <92376765+eleazarmgil@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const connectDB = require('./db')
+const User = require('./users');
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.get('/status', (req, res) => {
+    res.send('pong');
+});
+
+
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.get('/users/:id', async (req, res) => {    //http://localhost:3000/users/1
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) throw new Error('usuario no encontrado');
+        res.json(user);
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
+app.post('/users', async (req, res) => {    //http://localhost:3000/users/1
+    try {
+        const { nombre, apellido, edad } = req.body;
+        const user = new User({ nombre, apellido, edad });
+        await user.save();
+        res.json({ success: true });
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.put('/users/:id', async (req, res) => {    //http://localhost:3000/users/1
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!user) throw new Error('User not found');
+        res.json({ success: true });
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        if (!user) throw new Error('User not found');
+
+      
+        if (req.body.nombre) user.nombre = req.body.nombre;
+        if (req.body.apellido) user.apellido = req.body.apellido;
+        if (req.body.edad) user.edad = req.body.edad;
+
+
+        await user.save();
+
+        res.json({ success: true });
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
+app.delete('/users/:id', async (req, res) => {    //http://localhost:3000/users/1
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) throw new Error('User not found');
+        res.json({ success: true });
+    }
+    catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log("API server started on port 5000");
+})
---
+++ users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const usersSchema = new mongoose.Schema(
+    {
+        nombre: {
+            type: String,
+            required: true
+        },
+        apellido: {
+            type: String,
+            required: true
+        },
+        Edad: {
+            type: Number,
+            required: true
+        }
+    });
+
+const User = mongoose.model('User', usersSchema);
+
+module.exports = User;
